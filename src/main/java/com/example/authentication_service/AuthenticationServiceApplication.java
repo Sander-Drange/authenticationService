39632d41ab9073b0adce9c98153e1dd92f121ab2
Sander-Drange <sadr002@student.kristiania.no>
2024-11-06 02:01:21 +0100

    fixed pom issue

--- src/main/java/com/example/authentication_service/AuthenticationServiceApplication.java
+++ src/main/java/com/example/authentication_service/AuthenticationServiceApplication.java
@@ -1,4 +1,4 @@
-package java.com.example.authentication_service;
+package com.example.authentication_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
